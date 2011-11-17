b5f8a6d65a2720c88cec690b6d228c9f1d8bc850
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2011-11-17 08:15:03 +0100

    Define the constants group xpath_results, node_types and document_positions

--- lib/Gtk3/WebKit.pm
+++ lib/Gtk3/WebKit.pm
@@ -37,21 +37,116 @@ This module provides the Perl bindings for the Gtk3 port of WebKit.
 
 use warnings;
 use strict;
+use base 'Exporter';
 
 use Glib::Object::Introspection;
 
 our $VERSION = '0.02';
 
 
+use constant {
+    # XPath result types
+    ANY_TYPE                       => 0,
+    NUMBER_TYPE                    => 1,
+    STRING_TYPE                    => 2,
+    BOOLEAN_TYPE                   => 3,
+    UNORDERED_NODE_ITERATOR_TYPE   => 4,
+    ORDERED_NODE_ITERATOR_TYPE     => 5,
+    UNORDERED_NODE_SNAPSHOT_TYPE   => 6,
+    ORDERED_NODE_SNAPSHOT_TYPE     => 7,
+    ANY_UNORDERED_NODE_TYPE        => 8,
+    FIRST_ORDERED_NODE_TYPE        => 9,
+
+    # Node type
+    ELEMENT_NODE                   => 1,
+    ATTRIBUTE_NODE                 => 2,
+    TEXT_NODE                      => 3,
+    CDATA_SECTION_NODE             => 4,
+    ENTITY_REFERENCE_NODE          => 5,
+    ENTITY_NODE                    => 6,
+    PROCESSING_INSTRUCTION_NODE    => 7,
+    COMMENT_NODE                   => 8,
+    DOCUMENT_NODE                  => 9,
+    DOCUMENT_TYPE_NODE             => 10,
+    DOCUMENT_FRAGMENT_NODE         => 11,
+    NOTATION_NODE                  => 12,
+
+    # Document position
+    DOCUMENT_POSITION_DISCONNECTED => 0x01,
+    DOCUMENT_POSITION_PRECEDING    => 0x02,
+    DOCUMENT_POSITION_FOLLOWING    => 0x04,
+    DOCUMENT_POSITION_CONTAINS     => 0x08,
+    DOCUMENT_POSITION_CONTAINED_BY => 0x10,
+    DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC => 0x20,
+};
+
+# export nothing by default.
+# export functions and constants by request.
+our %EXPORT_TAGS = (
+    xpath_results => [qw{
+        ANY_TYPE
+        NUMBER_TYPE
+        STRING_TYPE
+        BOOLEAN_TYPE
+        UNORDERED_NODE_ITERATOR_TYPE
+        ORDERED_NODE_ITERATOR_TYPE
+        UNORDERED_NODE_SNAPSHOT_TYPE
+        ORDERED_NODE_SNAPSHOT_TYPE
+        ANY_UNORDERED_NODE_TYPE
+        FIRST_ORDERED_NODE_TYPE
+    }],
+
+    node_types => [qw{
+        ELEMENT_NODE
+        ATTRIBUTE_NODE
+        TEXT_NODE
+        CDATA_SECTION_NODE
+        ENTITY_REFERENCE_NODE
+        ENTITY_NODE
+        PROCESSING_INSTRUCTION_NODE
+        COMMENT_NODE
+        DOCUMENT_NODE
+        DOCUMENT_TYPE_NODE
+        DOCUMENT_FRAGMENT_NODE
+        NOTATION_NODE
+    }],
+
+    document_positions => [qw{
+        DOCUMENT_POSITION_DISCONNECTED
+        DOCUMENT_POSITION_PRECEDING
+        DOCUMENT_POSITION_FOLLOWING
+        DOCUMENT_POSITION_CONTAINS
+        DOCUMENT_POSITION_CONTAINED_BY
+        DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC
+    }],
+);
+our @EXPORT_OK = map { @$_ } values %EXPORT_TAGS;
+$EXPORT_TAGS{all} = \@EXPORT_OK;
+
 
 sub import {
-    my $class = shift;
-    my %args = @_;
+    my %setup = (
+        basename  => 'WebKit',
+        version   => '3.0',
+        package   => __PACKAGE__,
+    );
 
-    $args{basename} = 'WebKit'    unless exists $args{basename};
-    $args{version}  = '3.0'       unless exists $args{version};
-    $args{package}  = __PACKAGE__ unless exists $args{package};
-    Glib::Object::Introspection->setup(%args);
+    my @args;
+    for (my $i = 0; $i < @_; ++$i) {
+        my $arg = $_[$i];
+        if (exists $setup{$arg}) {
+            $setup{$arg} = $_[++$i];
+        }
+        else {
+            push @args, $arg;
+        }
+    }
+
+    Glib::Object::Introspection->setup(%setup);
+
+    # Pretend that we're calling Exporter's import
+    @_ = @args;
+    goto &Exporter::import;
 }
 
 1;
